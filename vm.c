7d887dbf561c3371b7122176723fb88bd3cef80f
Cliff Snyder <cliff.snyder@gmail.com>
2023-11-28 09:28:42 -0500

    organizational

--- vm.c
+++ vm.c
@@ -1,42 +1,6 @@
+#include "lc3.h"
 #include <stdint.h> // for uint16_t
 
-#define MEMORY_MAX (1 << 16)
 uint16_t memory[MEMORY_MAX];
-
-// registers
-enum
-{
-  R_R0 = 0,
-  R_R1,
-  R_R2,
-  R_R3,
-  R_R4,
-  R_R5,
-  R_R6,
-  R_R7,
-  R_PC, /* program counter */
-  R_COND,
-  R_COUNT
-};
 uint16_t registers[R_COUNT];
 
-// op codes
-enum
-{
-  OP_BR = 0, /* branch */
-  OP_ADD,    /* add  */
-  OP_LD,     /* load */
-  OP_ST,     /* store */
-  OP_JSR,    /* jump register */
-  OP_AND,    /* bitwise and */
-  OP_LDR,    /* load register */
-  OP_STR,    /* store register */
-  OP_RTI,    /* unused */
-  OP_NOT,    /* bitwise not */
-  OP_LDI,    /* load indirect */
-  OP_STI,    /* store indirect */
-  OP_JMP,    /* jump */
-  OP_RES,    /* reserved (unused) */
-  OP_LEA,    /* load effective address */
-  OP_TRAP    /* execute trap */
-};
