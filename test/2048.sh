c56d7a609236f4960c7526a8d2202cc8d8f97ea7
Cliff Snyder <cliff.snyder@gmail.com>
2023-12-03 14:32:50 -0500

    got 2048 working!

---
+++ test/2048.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+set -euxo pipefail
+
+# TODO maybe do this with hexdump to give some more information about what went sideways
+# which hexdump >& /dev/null || { echo "can't find hexdump; skipping test..."; exit 77; }
+
+# if unset we'll expect our input to reside in the directory alongside our script
+DIR=$(dirname "$0")
+SRCDIR=${SRCDIR:-$DIR/..}
+BUILDDIR=${BUILDDIR:-$DIR/..}
+
+"$BUILDDIR/lc3as" -i "$SRCDIR/test/2048.asm" -o- | diff "$SRCDIR/test/2048.obj" -
