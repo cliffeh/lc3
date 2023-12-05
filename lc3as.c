d75d9f1ed601e511151dac6a56781593c9152868
Cliff Snyder <cliff.snyder@gmail.com>
2023-12-04 23:28:38 -0500

    cleanup and error messages

--- lc3as.c
+++ lc3as.c
@@ -168,37 +168,32 @@ main (int argc, const char *argv[])
 
   if (rc == 0)
     {
-      // TODO put this into a function
-      // for (symbol *sym = prog->symbols; sym; sym = sym->next)
-      //   {
-      //     printf ("%s: %04x\n", sym->label, sym->pos);
-      //   }
       rc = resolve_symbols (prog);
       if (rc)
-        exit (rc); // TODO error message?
+        {
+          fprintf (stderr, "%d symbols unresolved; bailing...\n", rc);
+          exit (rc);
+        }
 
       if (!flags) // we're supposed to output code
         {
           uint16_t tmp16 = swap16 (prog->orig);
           if (fwrite (&tmp16, sizeof (uint16_t), 1, out) != 1)
-            exit (1); // TODO error message
+            {
+              fprintf (stderr, "write error; bailing...\n");
+              exit (1);
+            }
           for (instruction *inst = prog->instructions; inst; inst = inst->next)
             {
               tmp16 = swap16 (inst->inst);
               if (fwrite (&tmp16, sizeof (uint16_t), 1, out) != 1)
-                exit (1); // TODO error message?
+                {
+                  fprintf (stderr, "write error; bailing...\n");
+                  exit (1);
+                }
             }
         }
-      // if ((rc = generate_code (out, prog, flags)) != 0)
-      //   {
-      //     fprintf (stderr, "%i errors found\n", rc);
-      //   }
       // TODO free_program()
-    }
-  else
-    {
-      // TODO better error handling/messages
-      fprintf (stderr, "there was an error\n");
     }
 
   exit (rc);
