f7857d2efbaa0652d717fbdf12c178f5d295c7bb
Cliff Snyder <cliff.snyder@gmail.com>
2023-12-05 19:55:19 -0500

    sign_extend refactor

--- lc3vm.c
+++ lc3vm.c
@@ -132,6 +132,7 @@ mem_read (uint16_t address)
 int
 main (int argc, const char *argv[])
 {
+  // TODO popt/add'l args
   if (argc < 2)
     {
       /* show usage string */
@@ -182,7 +183,7 @@ main (int argc, const char *argv[])
 
             if (imm_flag)
               {
-                uint16_t imm5 = sign_extend (instr & 0x1F, 5);
+                uint16_t imm5 = SIGN_EXT (instr & 0x1F, 5);
                 reg[r0] = reg[r1] + imm5;
               }
             else
@@ -202,7 +203,7 @@ main (int argc, const char *argv[])
 
             if (imm_flag)
               {
-                uint16_t imm5 = sign_extend (instr & 0x1F, 5);
+                uint16_t imm5 = SIGN_EXT (instr & 0x1F, 5);
                 reg[r0] = reg[r1] & imm5;
               }
             else
@@ -224,7 +225,7 @@ main (int argc, const char *argv[])
           break;
         case OP_BR:
           {
-            uint16_t pc_offset = sign_extend (instr & 0x1FF, 9);
+            uint16_t pc_offset = SIGN_EXT (instr & 0x1FF, 9);
             uint16_t cond_flag = (instr >> 9) & 0x7;
             if (cond_flag & reg[R_COND])
               {
@@ -245,7 +246,7 @@ main (int argc, const char *argv[])
             reg[R_R7] = reg[R_PC];
             if (long_flag)
               {
-                uint16_t long_pc_offset = sign_extend (instr & 0x7FF, 11);
+                uint16_t long_pc_offset = SIGN_EXT (instr & 0x7FF, 11);
                 reg[R_PC] += long_pc_offset; /* JSR */
               }
             else
@@ -258,7 +259,7 @@ main (int argc, const char *argv[])
         case OP_LD:
           {
             uint16_t r0 = (instr >> 9) & 0x7;
-            uint16_t pc_offset = sign_extend (instr & 0x1FF, 9);
+            uint16_t pc_offset = SIGN_EXT (instr & 0x1FF, 9);
             reg[r0] = mem_read (reg[R_PC] + pc_offset);
             update_flags (r0);
           }
@@ -268,7 +269,7 @@ main (int argc, const char *argv[])
             /* destination register (DR) */
             uint16_t r0 = (instr >> 9) & 0x7;
             /* PCoffset 9*/
-            uint16_t pc_offset = sign_extend (instr & 0x1FF, 9);
+            uint16_t pc_offset = SIGN_EXT (instr & 0x1FF, 9);
             /* add pc_offset to the current PC, look at that memory location to
              * get the final address */
             reg[r0] = mem_read (mem_read (reg[R_PC] + pc_offset));
@@ -279,7 +280,7 @@ main (int argc, const char *argv[])
           {
             uint16_t r0 = (instr >> 9) & 0x7;
             uint16_t r1 = (instr >> 6) & 0x7;
-            uint16_t offset = sign_extend (instr & 0x3F, 6);
+            uint16_t offset = SIGN_EXT (instr & 0x3F, 6);
             reg[r0] = mem_read (reg[r1] + offset);
             update_flags (r0);
           }
@@ -287,7 +288,7 @@ main (int argc, const char *argv[])
         case OP_LEA:
           {
             uint16_t r0 = (instr >> 9) & 0x7;
-            uint16_t pc_offset = sign_extend (instr & 0x1FF, 9);
+            uint16_t pc_offset = SIGN_EXT (instr & 0x1FF, 9);
             reg[r0] = reg[R_PC] + pc_offset;
             update_flags (r0);
           }
@@ -295,14 +296,14 @@ main (int argc, const char *argv[])
         case OP_ST:
           {
             uint16_t r0 = (instr >> 9) & 0x7;
-            uint16_t pc_offset = sign_extend (instr & 0x1FF, 9);
+            uint16_t pc_offset = SIGN_EXT (instr & 0x1FF, 9);
             mem_write (reg[R_PC] + pc_offset, reg[r0]);
           }
           break;
         case OP_STI:
           {
             uint16_t r0 = (instr >> 9) & 0x7;
-            uint16_t pc_offset = sign_extend (instr & 0x1FF, 9);
+            uint16_t pc_offset = SIGN_EXT (instr & 0x1FF, 9);
             mem_write (mem_read (reg[R_PC] + pc_offset), reg[r0]);
           }
           break;
@@ -310,7 +311,7 @@ main (int argc, const char *argv[])
           {
             uint16_t r0 = (instr >> 9) & 0x7;
             uint16_t r1 = (instr >> 6) & 0x7;
-            uint16_t offset = sign_extend (instr & 0x3F, 6);
+            uint16_t offset = SIGN_EXT (instr & 0x3F, 6);
             mem_write (reg[r1] + offset, reg[r0]);
           }
           break;
