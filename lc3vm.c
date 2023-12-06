b7cad7f805d06802eed72703358ec58eeb3c43e3
Cliff Snyder <cliff.snyder@gmail.com>
2023-12-05 19:59:33 -0500

    getting rid of fread errors

--- lc3vm.c
+++ lc3vm.c
@@ -76,13 +76,13 @@ read_image_file (FILE *file)
 {
   /* the origin tells us where in memory to place the image */
   uint16_t origin;
-  fread (&origin, sizeof (origin), 1, file);
+  size_t read = fread (&origin, sizeof (origin), 1, file);
   origin = SWAP16 (origin);
 
   /* we know the maximum file size so we only need one fread */
   uint16_t max_read = MEMORY_MAX - origin;
   uint16_t *p = memory + origin;
-  size_t read = fread (p, sizeof (uint16_t), max_read, file);
+  read += fread (p, sizeof (uint16_t), max_read, file);
 
   /* swap to little endian */
   while (read-- > 0)
