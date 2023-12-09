c14505d50c7982f43380e1c8f4d4e02d6b716bc1
Cliff Snyder <cliff.snyder@gmail.com>
2023-12-09 11:46:31 -0500

    replaced swap16

--- lc3vm.c
+++ lc3vm.c
@@ -55,10 +55,7 @@ uint16_t sign_extend(uint16_t x, int bit_count)
     }
     return x;
 }
-uint16_t swap16(uint16_t x)
-{
-    return (x << 8) | (x >> 8);
-}
+
 void update_flags(uint16_t r)
 {
     if (reg[r] == 0)
@@ -79,7 +76,7 @@ void read_image_file(FILE* file)
     /* the origin tells us where in memory to place the image */
     uint16_t origin;
     fread(&origin, sizeof(origin), 1, file);
-    origin = swap16(origin);
+    origin = SWAP16(origin);
 
     /* we know the maximum file size so we only need one fread */
     uint16_t max_read = MEMORY_MAX - origin;
@@ -89,7 +86,7 @@ void read_image_file(FILE* file)
     /* swap to little endian */
     while (read-- > 0)
     {
-        *p = swap16(*p);
+        *p = SWAP16(*p);
         ++p;
     }
 }
