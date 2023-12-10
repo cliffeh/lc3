768b0bd75a0b4a3c1fcdad368828aa1880eaf3fe
Cliff Snyder <cliff.snyder@gmail.com>
2023-12-09 21:49:40 -0500

    oops

---
+++ assemble.c
@@ -0,0 +1,39 @@
+#include "lc3as.h"
+
+int
+resolve_symbols (program *prog, instruction *inst)
+{
+  if (inst->sym)
+    {
+      if (!inst->sym->is_set)
+        {
+          fprintf (stderr, "error: unresolved symbol: %s\n", inst->sym->label);
+          return 1;
+        }
+
+      if (inst->flags)
+        inst->inst |= (((inst->sym->addr - inst->addr) - 1) & inst->flags);
+      else
+        inst->inst = inst->sym->addr + prog->orig;
+    }
+
+  return 0;
+}
+
+int
+assemble_program (uint16_t dest[], FILE *in)
+{
+  program prog = { .orig = 0, .len = 0, .instructions = 0, .symbols = 0 };
+
+  parse_program (&prog, in);
+
+  int i = 0;
+  dest[i++] = SWAP16 (prog.orig);
+  for (instruction *inst = prog.instructions; inst; inst = inst->next)
+    {
+      resolve_symbols (&prog, inst);
+      dest[i++] = SWAP16 (inst->inst);
+    }
+    
+  return i;
+}
